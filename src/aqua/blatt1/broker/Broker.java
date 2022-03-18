c61bd55366d01ff689a927afa409bd89f073168f
Simon <Simon.Kaemmer@yahoo.de>
2022-03-19 00:56:45 +0100

    Adding functionality

--- src/aqua/blatt1/broker/Broker.java
+++ src/aqua/blatt1/broker/Broker.java
@@ -1,14 +1,27 @@
 package aqua.blatt1.broker;
 
 import messaging.Endpoint;
-import
+import messaging.Message;
 
 public class Broker {
 
     private Endpoint ep;
+    private ClientCollection<java.net.InetSocketAddress> cc;
 
     public Broker() {
         ep = new Endpoint(4711);
+        cc = new ClientCollection<>();
+    }
+
+    public void broker() {
+        while(true){
+            Message msg = ep.blockingReceive();
+        }
+    }
+
+    public void register() {
+        String id = "tank" + cc.size();
+
     }
 
 }
