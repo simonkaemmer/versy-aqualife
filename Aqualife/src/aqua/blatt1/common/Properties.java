7379723b8f007ae9b95f71df4b8d5bf131baaae6
Lukas Streinu <lukas.streinu@htwg-konstanz.de>
2022-04-03 16:04:34 +0200

    refactor project and started blatt2

--- Aqualife/src/aqua/blatt1/common/Properties.java
+++
@@ -1,7 +0,0 @@
-package aqua.blatt1.common;
-
-public class Properties {
-	public final static String HOST = "localhost";
-	public final static int PORT = 4711;
-	public final static String BROKER_NAME = "AquaBroker";
-}
--- Aqualife/src/aqua/blatt1/common/msgtypes/RegisterRequest.java
+++
@@ -1,8 +0,0 @@
-package aqua.blatt1.common.msgtypes;
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public final class RegisterRequest implements Serializable {
-
-}
